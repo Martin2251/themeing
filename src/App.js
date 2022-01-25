4c43b0ee7ea3102b9738fe2ca88a6bae0c1a5b1b
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-01-25 08:05:01 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react"
+import styled, {ThemeProvider} from "styled-components";
+
+
+const themeDefault = {
+colorPrimary: "rebeccapurple",
+};
+
+const themeDark = {
+ id: "dark",
+  colorBackground: "black",
+  colorText: "white"
+};
+
+const themeLight = {
+  id: "light",
+  colorBackground: "white", 
+  colorText: "black"
+};
+
+const StyledWrapper = styled.div`
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+background: ${p => p.theme.colorBackground};
+color: ${p => p.theme.colorText};
+transition : background 0.35s;
+`
+
+const StyledButton = styled.button`
+background: ${(p) => p.theme.colorPrimary};
+color: white;
+`;
 
 function App() {
+
+
+  const [theme, setTheme] = useState(themeDefault);
+
+  const handleThemeToggled = () => {
+    if (theme.id === "dark") {
+      setTheme({
+        ...themeDefault,
+        ...themeLight,
+      })
+    } else {
+      setTheme({
+        ...themeDefault,
+        ...themeDark,
+      })
+    }
+  }
+
+useEffect(() => {
+  setTheme({
+    ...themeDefault, ...themeLight
+  })
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <ThemeProvider theme={theme}>
+    <StyledWrapper>
+ <StyledButton onChangeClick ={handleThemeToggled}>Toggle Theme</StyledButton>
+ </StyledWrapper>
+ </ThemeProvider>
   );
 }
 
